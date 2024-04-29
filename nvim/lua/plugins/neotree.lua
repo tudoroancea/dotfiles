caebbb085ff96ef28242c8b0d93e490d19a6fe76
Tudor Oancea <oancea@embotech.com>
2024-04-29 15:22:46 +0200

    add .zshrc and .tmux.conf to neotree not hidden

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -2,7 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, config)
     config.filesystem.filtered_items = {
-      always_show = { ".gitignore", ".gitmodules", ".venv", ".vscode", ".github", ".idea" },
+      always_show = { ".gitignore", ".gitmodules", ".venv", ".vscode", ".github", ".idea", ".zshrc", ".tmux.conf" },
     }
     -- require("neo-tree").setup {
     --   filesystem = {
