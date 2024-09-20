1ad6ed91d4f9995bdccc3783a94e6a606fa03435
Ted <oancea.tudor@icloud.com>
2024-09-20 10:39:36 +0200

    switch to light theme

--- nvim/lua/plugins/astroui.lua
+++ nvim/lua/plugins/astroui.lua
@@ -9,7 +9,8 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "dracula",
+    -- colorscheme = "dracula",
+    colorscheme = "rose-pine-dawn",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
