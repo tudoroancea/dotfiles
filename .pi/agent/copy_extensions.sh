aed15837c1a4c96926ccae5b1c2484c1d2302e28
Tudor Oancea <oancea.tudor@icloud.com>
2026-03-01 14:15:59 +0100

    remove old pi extensions

--- .pi/agent/copy_extensions.sh
+++
@@ -1,6 +0,0 @@
-cp ~/dev/pi-mono/packages/coding-agent/examples/extensions/todo.ts ~/.pi/agent/extensions/todo.ts
-cp ~/dev/pi-mono/packages/coding-agent/examples/extensions/question.ts ~/.pi/agent/extensions/question.ts
-cp ~/dev/pi-mono/packages/coding-agent/examples/extensions/questionnaire.ts ~/.pi/agent/extensions/questionnaire.ts
-cp ~/dev/pi-mono/packages/coding-agent/examples/extensions/handoff.ts ~/.pi/agent/extensions/handoff.ts
-cp ~/dev/pi-mono/packages/coding-agent/examples/extensions/snake.ts ~/.pi/agent/extensions/snake.ts
-cp ~/dev/pi-mono/packages/coding-agent/examples/extensions/mac-system-theme.ts ~/.pi/agent/extensions/mac-system-theme.ts
